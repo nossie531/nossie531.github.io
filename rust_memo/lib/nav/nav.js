80edeb022a881a3c47b722997fbf0c96cf43f06f
okd <nossie531@gmail.com>
2024-05-12 07:06:40 +0900

    Update #1

--- rust_memo/lib/nav/nav.js
+++ rust_memo/lib/nav/nav.js
@@ -1,55 +1,109 @@
 "use strict";
 
 {
-	// ナビゲーションを管理するクラスです。
-	class Nav {
-
-		static #openText = "[≡]";
-		static #closeText = "[×]";
-
-		#sectionToAnchors = new Map;
-
+	// ナビゲーションの管理クラス。
+	class NavManager {
+		#mainNav;
 		#elements = {
 			root: this.#createRoot(),
 			head: this.#createHead(),
-			toggleLink: this.#createToggleLink(Nav.#openText),
+			toggleLink: this.#createToggleLink(NavUtil.getToggleText(true)),
 			pageTopLink: this.#createPageTopLink("[▲]"),
 			upLink: this.#createRelatedLink("up", "[↑]"),
 			prevLink: this.#createRelatedLink("prev", "[←]"),
 			nextLink: this.#createRelatedLink("next", "[→]"),
 		};
 
-		// 起動。
-		static exec() {
-			const nav = new Nav;
-			const setupNav = nav.#setupNav.bind(nav);
-			Util.addCallbackOnDomLoaded(setupNav);
+		// 各要素を構築。
+		constructor() {
+			this.#elements.root.append(this.#elements.head);
+			this.#elements.head.append(this.#elements.prevLink);
+			this.#elements.head.append(this.#elements.nextLink);
+			this.#elements.head.append(this.#elements.upLink);
+			this.#elements.head.append(this.#elements.pageTopLink);
+			this.#elements.head.append(this.#elements.toggleLink);
+			this.#mainNav = new NavMaker(this.#elements.root);
 		}
 
-		// nav 要素の設定。
-		async #setupNav() {
-			this.#buildElements();
-			this.#setupToggleAction();
-			this.#setupScrollAction();
-			await this.#setupRelatedLinks();
+		// 起動。
+		static exec() {
+			const nav = new NavManager;
+			nav.#setupToggleAction();
+			nav.#setupVerticalLinks();
+			nav.#setupRelatedLinks();
+			nav.#setupIndexHighlight();
+			nav.#startResponseObserve();
+		}
 
+		// レスポンスの監視を開始。
+		#startResponseObserve() {
+			const reader = new TreeStackReader(document.documentElement);
+			Util.addCallbackOnPageLoading(() => {
+				for (let act; act = reader.nextAction();) {
+					if (!NavUtil.getSectionHeader(act.elm)) {
+						continue;
+					}
+
+					if (!this.#elements.root.parentElement) {
+						this.#insertMainNav();
+					}
+
+					if (act.isPushed) {
+						this.#mainNav.pushElm(act.elm);
+					} else {
+						this.#mainNav.popElm();
+					}
+				}
+			});
+		}
+
+		// メイン目次の組込。
+		#insertMainNav() {
 			const fstSection = document.getElementsByTagName("section")[0];
 			if (fstSection) {
 				fstSection.before(this.#elements.root);
 			} else {
-				document.body.appendChild(this.#elements.root);
+				document.body.append(this.#elements.root);
 			}
 		}
 
-		// 関連リンクを設定。
-		async #setupRelatedLinks() {
-			const indexUrl = new URL("index.xhtml", document.URL);
-			const indexDoc = await Util.fetchXhtml(indexUrl.toString());
-			if (indexUrl.toString() !== (new URL(document.URL).toString())) {
-				this.#elements.upLink.href = indexUrl.toString();
-				setAnchorHref(this.#elements.prevLink, findOffsetUrl(document.URL, -1));
-				setAnchorHref(this.#elements.nextLink, findOffsetUrl(document.URL, +1));
+		// 縦方向移動系のリンクスタイルの更新。
+		#setupVerticalLinks() {
+			const self = this;
+			refresh();
+			window.addEventListener("scroll", refresh);
+			function refresh() {
+				const atPageTop = document.documentElement.scrollTop === 0;
+				self.#elements.upLink.classList.toggle("none", !atPageTop);
+				self.#elements.pageTopLink.classList.toggle("none", atPageTop);
 			}
+		}
+
+		// トグルボタン押下時の動作を設定。
+		#setupToggleAction() {
+			this.#elements.toggleLink.addEventListener("click", () => {
+				const isOpened = this.#elements.root.classList.contains("open");
+				this.#elements.root.classList.toggle("open");
+				this.#elements.toggleLink.textContent = NavUtil.getToggleText(isOpened);
+			});
+		}
+
+		// 関連リンクを設定。
+		#setupRelatedLinks() {
+			const indexUrl = new URL("index.xhtml", document.URL);
+			const promise = Util.fetchXhtml(indexUrl.toString());
+			promise.then((indexDoc) => {
+				if (indexUrl.toString() === (new URL(document.URL).toString())) {
+					return;
+				}
+
+				const prevUrl = findOffsetUrl(indexDoc, document.URL, -1);
+				const nextUrl = findOffsetUrl(indexDoc, document.URL, +1);
+				this.#elements.upLink.href = indexUrl.toString();
+				setAnchorHref(this.#elements.prevLink, prevUrl);
+				setAnchorHref(this.#elements.nextLink, nextUrl);
+			});
+
 
 			function setAnchorHref(anchor, url) {
 				if (url) {
@@ -57,7 +111,7 @@
 				}
 			}
 
-			function findOffsetUrl(href, offset) {
+			function findOffsetUrl(indexDoc, href, offset) {
 				if (!indexDoc || !href) {
 					return null;
 				}
@@ -68,76 +122,23 @@
 			}
 		}
 
-		// トグルボタン押下時の動作を設定。
-		#setupToggleAction() {
-			this.#elements.toggleLink.addEventListener("click", () => {
-				const isOpened = this.#elements.root.classList.contains("open");
-				this.#elements.root.classList.toggle("open");
-				this.#elements.toggleLink.textContent = !isOpened ? Nav.#closeText : Nav.#openText;
-			});
-		}
+		// 目次にスクロールに伴うハイライトを設定。
+		#setupIndexHighlight() {
+			const mainNav = this.#mainNav;
+			window.addEventListener("scroll", refresh);
 
-		// スクロール時の動作を設定。
-		#setupScrollAction() {
-			const self = this;
-			const sections = getH1Sections();
-			const indexElm = this.#elements.root.querySelector("ul.index");
-			window.addEventListener("scroll", () => scrollCallback.call(this));
-			scrollCallback.call(this);
-
-			function scrollCallback() {
-				updateUpOrTopLink.call(this);
-				if (indexElm) {
-					updateIndexHighlight();
+			function refresh() {
+				if (!mainNav.rootIndex) {
+					return;
 				}
-			}
 
-			function isInView(elm) {
-				const bcr = elm.getBoundingClientRect();
-				return bcr.bottom >= 0 && bcr.top <= window.innerHeight;
-			}
-
-			function isFirstView(elm) {
-				const prev = elm.previousElementSibling;
-				if (prev?.tagName === "section") {
-					return !isInView(prev) && isInView(elm);
+				const sectionRange = getSectionRange(mainNav.sections);
+				if (!sectionRange) {
+					mainNav.rootIndex.style.backgroundImage = "transparent";
 				} else {
-					const bcr = elm.getBoundingClientRect();
-					return bcr.top <= 0 && bcr.bottom >= 0;
-				}
-			}
-
-			function getH1Sections() {
-				const query = "section:has(> h1), section:has(> hgroup > h1)";
-				return Array.from(document.querySelectorAll(query));
-			}
-
-			function getSectionRange() {
-				const maxIndex = sections.findLastIndex(isInView);
-				const fstIndex = sections.slice(0, maxIndex + 1).findLastIndex(isFirstView);
-				const minIndex = maxIndex < 0 ? - 1 : Math.max(0, fstIndex);
-				return { isEmpty: maxIndex < 0, min: sections[minIndex], max: sections[maxIndex] };
-			}
-
-			function getAnchorElmFromSectionElm(sectionElm) {
-				const headerElm = sectionElm.querySelector("h1");
-				return indexElm.querySelector(`a[href=\"#${headerElm.id}\"]`);
-			}
-
-			function updateUpOrTopLink() {
-				const atPageTop = document.documentElement.scrollTop === 0;
-				this.#elements.upLink.classList.toggle("none", !atPageTop);
-				this.#elements.pageTopLink.classList.toggle("none", atPageTop);
-			}
-
-			function updateIndexHighlight() {
-				const sectionRange = getSectionRange();
-				if (sectionRange.isEmpty) {
-					indexElm.style.backgroundImage = "transparent";
-				} else {
-					const anchorMin = self.#sectionToAnchors.get(sectionRange.min);
-					const anchorMax = self.#sectionToAnchors.get(sectionRange.max);
-					const baseTop = indexElm.getBoundingClientRect().top;
+					const anchorMin = mainNav.getAnchorFrom(sectionRange.min);
+					const anchorMax = mainNav.getAnchorFrom(sectionRange.max);
+					const baseTop = mainNav.rootIndex.getBoundingClientRect().top;
 					const hlMin = anchorMin.getBoundingClientRect().top - baseTop;
 					const hlMax = anchorMax.getBoundingClientRect().bottom - baseTop;
 					const colors = { st: "transparent", hl: "var(--nav-hl-color)" };
@@ -150,17 +151,31 @@
 						{ color: colors.st, position: `100%` },
 					];
 					const grad = stops.map((x) => `${x.color} ${x.position}`).join(",");
-					indexElm.style.backgroundImage = `linear-gradient(${grad})`;
+					mainNav.rootIndex.style.backgroundImage = `linear-gradient(${grad})`;
 				}
-			};
-		}
+			}
 
-		// 要素を組み立て。
-		#buildElements() {
-			const e = this.#elements;
-			e.root.append(e.head);
-			e.head.append(e.prevLink, e.nextLink, e.upLink, e.pageTopLink, e.toggleLink);
-			e.root.append(this.#createIndex(document.body));
+			function getSectionRange(sections) {
+				const minIndex = getFirstViewIndex(sections);
+				const maxIndex = sections.findLastIndex(Util.isInScrollY);
+				return minIndex < 0 ? null : {
+					min: sections[minIndex],
+					max: sections[maxIndex]
+				};
+			}
+
+			function getFirstViewIndex(sections) {
+				const topIndex = Util.arrayWindow(sections, 2).findLastIndex(isTopView);
+				return topIndex >= 0 ? topIndex : sections.findIndex(Util.isInScrollY);
+			}
+
+			function isTopView(sectionPair) {
+				const currRect = sectionPair[0].getBoundingClientRect();
+				const nextRect = sectionPair[1]?.getBoundingClientRect();
+				const min = currRect.top;
+				const max = nextRect ? nextRect.top : currRect.bottom;
+				return min <= 0 && 0 <= max;
+			}
 		}
 
 		// ルート要素を生成。
@@ -207,58 +222,99 @@
 			result.append(text);
 			return result;
 		}
+	}
 
-		// 目次要素を生成。
-		#createIndex(elm) {
-			const sections = this.#getNavSections(elm);
-			if (sections.length === 0) {
-				return document.createDocumentFragment();
-			}
+	// 目次の生成装置。
+	class NavMaker {
+		#target;
+		#rootIndex;
+		#sectionToAnchors = new Map;
 
-			const ul = document.createElement("ul");
-			for (const section of sections) {
-				const children = this.#createIndex(section);
-				ul.append(this.#createIndexEntry(section, children));
-			}
-
-			ul.classList.toggle("index", elm.tagName === "body");
-			return ul;
+		constructor(root) {
+			this.#target = root;
 		}
 
-		// 目次の項目を一つ作成。
-		#createIndexEntry(section, children) {
-			const header = Nav.#getSectionHeader(section);
+		// ルート要素。
+		get rootIndex() {
+			return this.#rootIndex;
+		}
+
+		// 対象セクションの配列。
+		get sections() {
+			return Array.from(this.#sectionToAnchors, ([key, value]) => key);
+		}
+
+		// セクションに対応するリンクを取得。
+		getAnchorFrom(section) {
+			return this.#sectionToAnchors.get(section);
+		}
+
+		// 文書要素のプッシュ。
+		pushElm(elm) {
+			if (elm.tagName !== "section") {
+				return;
+			}
+
+			if (this.#target.tagName !== "ul") {
+				const ul = document.createElement("ul");
+				this.#target.append(ul);
+				this.#target = ul;
+				this.#rootIndex = this.#rootIndex || ul;
+			}
+
+			const header = NavUtil.getSectionHeader(elm);
+			const title = NavUtil.createTitlePart(header);
 			const li = document.createElement("li");
 			const a = document.createElement("a");
-			const vHeader = Nav.#adjustForHeader(Util.cloneContents(header));
-			const vChildren = children || document.createDocumentFragment();
-			const jumpTo = Nav.#jumpTo.bind(null, header);
-			this.#sectionToAnchors.set(section, a);
+			const jumpTo = NavUtil.jumpTo.bind(null, header);
 			a.tabIndex = 0;
 			a.href = "javascript:;"
 			a.addEventListener("click", jumpTo);
-			a.append(vHeader);
-			li.append(a, vChildren);
-			return li;
+			a.append(title);
+			li.append(a);
+			this.#target.append(li)
+			this.#target = li;
+			this.#sectionToAnchors.set(elm, a);
 		}
 
-		// 指定した要素の子孫から目次セクションを取得。
-		#getNavSections(elm) {
-			const result = [];
-			const tw = document.createTreeWalker(elm, NodeFilter.SHOW_ELEMENT);
+		// 文書要素のポップ。
+		popElm() {
+			this.#target = this.#target.parentNode;
+		}
+	}
 
-			for (let elm = tw.nextNode(); elm;) {
-				const scElm = elm.tagName === "section" ? elm : null;
-				const h1Elm = Nav.#getSectionHeader(elm);
-				result.push(...(h1Elm ? [scElm] : []));
-				elm = scElm ? tw.nextSibling() : tw.nextNode();
+	// ナビゲーション関連のユーティリティ。
+	class NavUtil {
+		static #openText = "[≡]";
+		static #closeText = "[×]";
+
+		// タイトル部を生成。
+		static createTitlePart(header) {
+			const contents = Util.cloneContents(header);
+			Util.removeAnchors(contents);
+			Util.trimTextNodes(contents);
+			return contents;
+		}
+
+		// トグルリンク用のテキストを取得。
+		static getToggleText(flag) {
+			return flag ? NavUtil.#openText : NavUtil.#closeText;
+		}
+
+		// セクション要素の見出しを取得。
+		static getSectionHeader(elm) {
+			const sectionElm = elm?.tagName === "section" ? elm : null;
+			const hgroupElm = getFirstElementIf(sectionElm, "hgroup");
+			return getFirstElementIf(hgroupElm || sectionElm, "h1");
+
+			function getFirstElementIf(elm, tagName) {
+				const fstElm = elm?.firstElementChild;
+				return fstElm?.tagName === tagName ? fstElm : null;
 			}
-
-			return result;
 		}
 
 		// 指定した要素へジャンプします。
-		static #jumpTo(elm) {
+		static jumpTo(elm) {
 			Util.clearUrlHash();
 			elm.scrollIntoView();
 			animation("none");
@@ -269,43 +325,106 @@
 				elm.style.animation = value;
 			}
 		}
+	}
 
-		// 要素の内容をヘッダ用にカスタマイズします。
-		static #adjustForHeader(elm) {
-			Util.removeAnchors(elm);
-			Util.trimTextNodes(elm);
-			return elm;
+	// 文書ツリーのスタックリーダー。
+	class TreeStackReader {
+		#lastElm;
+		#isPushed = true;
+		#isPending = false;
+		#isJustConstructed = true;
+
+		constructor(elm) {
+			this.#lastElm = elm;
 		}
 
-		// セクション要素の見出しを取得。
-		static #getSectionHeader(elm) {
-			const sectionElm = elm?.tagName === "section" ? elm : null;
-			const hgroupElm = getFirstElementIf(sectionElm, "hgroup");
-			return getFirstElementIf(hgroupElm || sectionElm, "h1");
+		// 次の動作を実行。
+		nextAction() {
+			if (this.#lastElm === null) {
+				return null;
+			}
 
-			function getFirstElementIf(elm, tagName) {
-				const fstElm = elm?.firstElementChild;
-				return fstElm?.tagName === tagName ? fstElm : null;
+			if (this.#isJustConstructed) {
+				this.#isJustConstructed = false;
+			} else if (this.#isPushed) {
+				const nextElm = this.#lastElm.firstElementChild;
+				this.#lastElm = nextElm || this.#lastElm;
+				this.#isPushed = !!nextElm;
+				this.#isPending = false;
+			} else {
+				const nextElm = this.#lastElm.nextElementSibling;
+				const isPending = !nextElm && !Util.isLoadDone(this.#lastElm.parentElement);
+				this.#lastElm = isPending ? this.#lastElm : (nextElm || this.#lastElm.parentElement);
+				this.#isPushed = isPending ? this.#isPushed : !!nextElm;
+				this.#isPending = isPending;
+			}
+
+			if (this.#isPending || this.#lastElm === null) {
+				return null;
+			} else {
+				return { elm: this.#lastElm, isPushed: this.#isPushed };
 			}
 		}
 	}
 
 	// ユーティリティクラス。
 	class Util {
-		// 文書のロード完了後にコールバック。
-		static addCallbackOnDomLoaded(f) {
-			if (document.readyState === "loading") {
-				document.addEventListener("DOMContentLoaded", f);
-			} else {
-				f();
-			}
-		}
-
 		// 要素の内容をクローン。
 		static cloneContents(elm) {
 			const range = document.createRange();
 			range.selectNodeContents(elm);
 			return range.cloneContents();
+		}
+
+		// 文書のロードが完了済かを取得。
+		static isDocLoadDone() {
+			return document.readyState !== "loading";
+		}
+
+		// 要素のロードが完了済かを取得。
+		static isLoadDone(elm) {
+			if (Util.isDocLoadDone()) {
+				return true;
+			}
+
+			for (let curr = elm; curr; curr = curr.parentNode) {
+				if (curr.nextElementSibling) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+			
+		// 要素がビューポートの縦スクロール内にあるかを取得。
+		static isInScrollY(elm) {
+			const rect = elm.getBoundingClientRect();
+			return rect.bottom >= 0 && rect.top <= window.innerHeight;
+		}
+
+		// 配列を指定サイズの重複した部分配列へと分解。
+		static arrayWindow(arr, size) {
+			return arr.map((_, i) => arr.slice(i, i + size));
+		}
+
+		// ページロード中のコールバックを登録。
+		static addCallbackOnPageLoading(f) {
+			if (Util.isDocLoadDone()) {
+				f();
+				return;
+			}
+
+			const observer = new MutationObserver(callback);
+			const root = document.documentElement;
+			const options =  { childList: true, subtree: true };
+			observer.observe(root, options);
+
+			function callback(_records, observer) {
+				f();
+				if (Util.isDocLoadDone()) {
+					observer.disconnect();
+				}
+			}
 		}
 
 		// 現在の URL からハッシュ部分を削除。
@@ -346,5 +465,5 @@
 		}
 	}
 
-	Nav.exec();
+	NavManager.exec();
 }
