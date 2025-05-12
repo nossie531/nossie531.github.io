bd8013159bd8f845493a36657580df83a3c54fb7
okd <nossie531@gmail.com>
2025-05-12 19:03:33 +0900

    Update: 引用の仕組

--- doc/rust/lib/base.js
+++ doc/rust/lib/base.js
@@ -4,7 +4,6 @@
 	/* -- スクリプトのロード --
 	 - Chrome のバグで XML 形式からは import 宣言が無効 (import 構文は有効)
 	 - https://issues.chromium.org/issues/40518469 */
-	import("../../../lib/quote.js");
 	import("../../../lib/prism/prism.js").then(() => setupPrismJs());
 
 	// このサイトの管理用の基本クラス。
@@ -16,7 +15,6 @@
 			this.#instance.#setupGoogleAnalytics();
 			document.addEventListener("DOMContentLoaded", () => {
 				this.#instance.#setupCodes();
-				this.#instance.#setupIframes();
 			});
 		}
 		
@@ -52,59 +50,6 @@
 			function gtag() {
 				window.dataLayer.push(arguments);
 			}
-		}
-
-		/* iframe の高さを内容に依存するよう設定。
-		 - 欠陥: 印刷時の倍率指定が等倍以外だとレイアウトが崩れる。
-		 - 欠陥: 印刷時のメディアクエリの変化に追随できない。
-		 - 後者の制限は印刷系イベントの活用で迂回できる。*/
-		#setupIframes() {
-			const observer = new ResizeObserver(onResize);
-			const iframes = document.querySelectorAll("iframe.autoHeight");
-			for (const iframe of iframes) {
-				Util.afterIframeLoaded(iframe, onDomLoaded);
-			}
-
-			function onDomLoaded(iframe) {
-				adjustIframeHeight(iframe);
-				observer.observe(iframe.contentDocument.documentElement);
-			}
-
-			function onResize(entries) {
-				for (const entry of entries) {
-					const iframe = entry.target.ownerDocument.defaultView.frameElement;
-					adjustIframeHeight(iframe);
-				}
-			}
-
-			function adjustIframeHeight(iframe) {
-				const root = iframe.contentDocument.documentElement;
-				const height = Math.ceil(root.getBoundingClientRect().height);
-				iframe.style.height = height + "px";
-			}
-		}
-	}
-
-	// ユーティリティクラス。
-	class Util {
-		// iframe のロード後にコールバックを設定。
-		static afterIframeLoaded(iframe, callback) {
-			const exec = () => callback(iframe);
-			if (Util.#isIframeLoading(iframe)) {
-				iframe.contentWindow.addEventListener("DOMContentLoaded", exec, true);
-			} else {
-				exec();
-			}
-		}
-
-		/* iframe が内容をロード中かを取得。
-		 - Chrome や Firefox は iframe の初期状態でダミー文書を持つ。
-		 - https://issues.chromium.org/issues/41443865 */
-		static #isIframeLoading(iframe) {
-			const doc = iframe.contentDocument;
-			const isLoading = doc.readyState === "loading";
-			const isDummyDoc = doc.body.children.length === 0;
-			return isLoading || isDummyDoc;
 		}
 	}
 
